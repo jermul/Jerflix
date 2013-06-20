41c0f685acdf44443a1dd782127d1f5d195a96bb
Jeremy Muljana <jermul714@gmail.com>
2013-06-19 18:26:16 -0700

    finish module 4

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,13 @@ Myflix::Application.routes.draw do
   resources :categories, 	only: [:show]
   resources :users, 			only: [:create]
   resources :sessions,    only: [:create]
+  resources :queue_items, only: [:create, :destroy]
 
   get 'home',     to: 'videos#index'
   get 'register', to: 'users#new'
   get 'sign_in',  to: 'sessions#new'
   get 'sign_out', to: 'sessions#destroy'
+  get 'my_queue', to: 'queue_items#index'
 
   get 'ui(/:action)', controller: 'ui'
 
